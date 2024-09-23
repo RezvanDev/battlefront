4c315d0fb8db648cc414f530cd1e9c7810137428
max <max@MacBook-Air-max.local>
2024-09-23 14:55:12 +0500

    mddddf

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,12 @@ declare module '*.png' {
     const value: string;
     export default value;
   }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+
+  interface ImportMetaEnv {
+    readonly VITE_API_URL: string
+    // здесь можно добавить другие переменные окружения
+  }
